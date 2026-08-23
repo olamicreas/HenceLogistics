c100d6f3eec55f823e24dad62d252f5339b191db
olamicreas <abdulquayyumoyedotun@gmail.com>
2026-08-23 01:27:17 +0100

    Capture raw xcodebuild log to artifact to bypass xcpretty ReDoS hang

--- patch-expo.js
+++ patch-expo.js
@@ -6,6 +6,15 @@ if (fs.existsSync(file)) {
   // 1. Inject sandboxing flag
   content = content.replace(/xcodebuild \\/g, 'xcodebuild ENABLE_USER_SCRIPT_SANDBOXING=NO \\');
   
+  // 2. Remove quiet flag to see output if it's there
+  content = content.replace(/-quiet \\/g, ' \\');
+  
+  // 3. Capture output
+  const target = 'SWIFT_COMPILATION_MODE=wholemodule \\\n  )';
+  const replacement = 'SWIFT_COMPILATION_MODE=wholemodule \\\n  ) > /tmp/expo_build.log 2>&1';
+  
+  content = content.replace(target, replacement);
+  
   fs.writeFileSync(file, content);
   console.log('Patched ExpoModulesJSI build script successfully.');
 }
